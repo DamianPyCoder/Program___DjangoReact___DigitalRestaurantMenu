cace0d9fdf9e012019717bb301073c28fc537519
Damian <damia_molin@hotmail.com>
2024-05-26 00:14:50 +0200

    subida de proytecto

---
+++ react/src/utils/constants.js
@@ -0,0 +1,12 @@
+export const BASE_API = "https://icard-django.herokuapp.com";
+export const TOKEN = "token";
+
+export const ORDER_STATUS = {
+  PENDING: "PENDING",
+  DELIVERED: "DELIVERED",
+};
+
+export const PAYMENT_STATUS = {
+  PENDING: "PENDING",
+  PAID: "PAID",
+};
